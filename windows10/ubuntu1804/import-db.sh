02eaeab50a7f239a4a5b7c170cfd9b596c6bd500
Ping Zhang <pz@blockcollider.org>
2021-07-13 22:43:22 -0700

    remove color in echo since it is not friendly for web ui

--- windows10/ubuntu1804/import-db.sh
+++ windows10/ubuntu1804/import-db.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-NC='\033[0m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-
 if [ $# -eq 0 ]
   then
-    echo -e "${RED}Error: No arguments supplied".
+    echo -e "Error: No arguments supplied".
     echo -e "Usage: $0 /path/to/bcnode-db-2029-11-12_13-14-15.tar.gz"
-    echo -e "       $0 /path/to/_easysync_db.zip${NC}"
+    echo -e "       $0 /path/to/_easysync_db.zip"
     exit 1
 fi
 
@@ -19,49 +14,49 @@ bcnode_container_name=bcnode
 database_volume_name=db
 
 if [ ! -f "${database_location}" ]; then
-  echo -e "${RED}Error: Missing import file ${database_location}${NC}"
+  echo -e "Error: Missing import file ${database_location}"
   exit 1
 fi
 
-echo -e "${GREEN}Preparing to import the blockchain database from ${database_location}...${NC}"
+echo -e "Preparing to import the blockchain database from ${database_location}..."
 docker rm -f importdb > /dev/null 2>&1 || true
 
 read -p "This will stop the container if ${bcnode_container_name} is currently running. Are you sure? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
-    echo -e "${GREEN}This was a close call man!${NC}"
+    echo -e "This was a close call man!"
     exit 1
 fi
 
 if [ -f "./overline_gpu_miner" ]; then
     ./overline_gpu_miner clean
 else
-  echo -e "${GREEN}Stopping current ${bcnode_container_name} container if running...${NC}"
+  echo -e "Stopping current ${bcnode_container_name} container if running..."
   docker rm -f ${bcnode_container_name} > /dev/null 2>&1 || true
 fi
 
 existing_volume=$(docker volume ls -q -f name=${database_volume_name} | grep -w ${database_volume_name}) || true
 if [ -z ${existing_volume} ]; then
-  echo -e "${YELLOW}Warning: No named Docker volume \"${database_volume_name}\" found, creating it..."
-  echo -e "Make sure it's going to be attached to the ${bcnode_container_name} container!${NC}"
+  echo -e "Warning: No named Docker volume \"${database_volume_name}\" found, creating it..."
+  echo -e "Make sure it's going to be attached to the ${bcnode_container_name} container!"
   docker volume create ${database_volume_name}
 else
   read -p "This will delete the local blockchain copy in Docker volume ${database_volume_name}. Are you sure? " -n 1 -r
   echo    # (optional) move to a new line
   if [[ ! $REPLY =~ ^[Yy]$ ]]
   then
-      echo -e "${GREEN}This was a close call man!${NC}"
+      echo -e "This was a close call man!"
       exit 1
   fi
   docker volume rm ${database_volume_name}
 fi
 
-echo -e "${GREEN}Starting dummy container to access ${database_volume_name} volume...${NC}"
+echo -e "Starting dummy container to access ${database_volume_name} volume..."
 docker run -d --rm --name importdb -v ${database_volume_name}:/root alpine tail -f /dev/null
 
 tmp_dir=`mktemp -d`
-echo -e "${GREEN}Extracting database $1 to ${tmp_dir}${NC}"
+echo -e "Extracting database $1 to ${tmp_dir}"
 if [ ${1: -3} == ".gz" ]; then
   tar -xf $1 -C ${tmp_dir}
 else
@@ -72,14 +67,14 @@ fi
 rm ${tmp_dir}/_data/db/IDENTITY > /dev/null 2>&1 || true
 rm ${tmp_dir}/_data/.chainstate.db > /dev/null 2>&1 || true
 
-echo -e "${GREEN}Latest database timestamp:"
+echo -e "Latest database timestamp:"
 ls -Artls ${tmp_dir}/_data/db/*.sst | tail -n 1
 
-echo -e "${GREEN}Copying database to volume...${NC}"
+echo -e "Copying database to volume..."
 docker cp ${tmp_dir}/_data/* importdb:/root
 
-echo -e "${GREEN}Cleaning up...${NC}"
+echo -e "Cleaning up..."
 docker rm -f importdb > /dev/null 2>&1
 rm -rf ${tmp_dir}
 
-echo -e "${GREEN}Done."
+echo -e "Done."
